8848f31969dc196ce2d0cc0640bd6305b973310f
Sturmiluli <noahschbern@hotmail.com>
2022-04-10 12:05:56 +0200

    Not Working yet Other targets

--- src/main/java/com/sturmiluli/flyplugin/commands/FlyCommand.java
+++ src/main/java/com/sturmiluli/flyplugin/commands/FlyCommand.java
@@ -1,5 +1,6 @@
 package com.sturmiluli.flyplugin.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,24 +14,30 @@ public class FlyCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player){
+        if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (args.length == 0 {
-                if(player.hasPermission("flyplugin.fly")){
-                    if (list_of_flying_players.contains(player)){
-                        list_of_flying_players.remove(player);
-                        player.setAllowFlight(false);
-                        player.sendMessage(ChatColor.AQUA + "Fly Disabled");
-                    }else if(!list_of_flying_players.contains(player)){
-                        list_of_flying_players.add(player);
-                        player.setAllowFlight(true);
-                        player.sendMessage(ChatColor.AQUA + "Fly Enabled");
-                    }
+            if (args.length == 0) {
+                flyMethod(player);
             }
+        } else if (args.length == 1) {
+            Player target = Bukkit.getPlayer(args[0]);
+            flyMethod(target);
 
+            return true;
+        }
+        return true; //DELETE THIS FUCKING SHIT
+    }
+    private void flyMethod(Player player){
+        if (player.hasPermission("flyplugin.fly")) {
+            if (list_of_flying_players.contains(player)) {
+                list_of_flying_players.remove(player);
+                player.setAllowFlight(false);
+                player.sendMessage(ChatColor.AQUA + "Fly Disabled");
+            } else if (!list_of_flying_players.contains(player)) {
+                list_of_flying_players.add(player);
+                player.setAllowFlight(true);
+                player.sendMessage(ChatColor.AQUA + "Fly Enabled");
             }
-        }else if(args.length == 1)
-
-        return true;
+        }
     }
 }
