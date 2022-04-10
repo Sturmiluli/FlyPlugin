5eaa530f765ad6474cb8cef052089c8fdae1a4aa
TeluTrix <lucaschbern@hotmail.com>
2022-04-10 19:21:05 +0100

    Fixed FlyPlugin argument problem

--- src/main/java/com/sturmiluli/flyplugin/commands/FlyCommand.java
+++ src/main/java/com/sturmiluli/flyplugin/commands/FlyCommand.java
@@ -10,28 +10,28 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 
 public class FlyCommand implements CommandExecutor {
-    private ArrayList<Player> list_of_flying_players = new ArrayList<>();
+    private ArrayList<Player> listOfFlyingPlayers = new ArrayList<>();
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 0) {
                 flyMethod(player);
+            } else if (args.length != 0) {
+                Player target = Bukkit.getPlayer(args[0]);
+                flyMethod(target);
             }
-        } else if (args.length == 1) {
-            Player target = Bukkit.getPlayer(args[0]);
-            flyMethod(target);
         }
         return true;
     }
     private void flyMethod(Player player){
         if (player.hasPermission("flyplugin.fly")) {
-            if (list_of_flying_players.contains(player)) {
-                list_of_flying_players.remove(player);
+            if (listOfFlyingPlayers.contains(player)) {
+                listOfFlyingPlayers.remove(player);
                 player.setAllowFlight(false);
                 player.sendMessage(ChatColor.AQUA + "Fly Disabled");
-            } else if (!list_of_flying_players.contains(player)) {
-                list_of_flying_players.add(player);
+            } else if (!listOfFlyingPlayers.contains(player)) {
+                listOfFlyingPlayers.add(player);
                 player.setAllowFlight(true);
                 player.sendMessage(ChatColor.AQUA + "Fly Enabled");
             }
